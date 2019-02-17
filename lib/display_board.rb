d658cf40f6b582be00cfa75981688676d9bcb9b7
John Cho <johnycho16@gmail.com>
2019-02-17 12:23:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,9 +4,9 @@ board=[" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
 	puts " #{board[0]} | #{board[1]} | #{board[2]} "
-	puts "---------"
+	puts "-----------"
 	puts " #{board[3]} | #{board[4]} | #{board[5]} "
-	puts "---------"
+	puts "-----------"
 	puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
